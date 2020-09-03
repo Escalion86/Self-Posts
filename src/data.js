edfd8a06b751b93756cd356f958285daa49d92c7
Aleksei Belinskiy <escalion86@gmail.com>
2020-09-04 00:27:27 +0700

    format code

--- src/data.js
+++ src/data.js
@@ -1,42 +1,42 @@
 export const DATA = [
   {
-    id: '1',
+    id: "1",
     img:
-      'https://static.coindesk.com/wp-content/uploads/2019/01/shutterstock_1012724596-860x430.jpg',
-    text: 'Awesome text for post 1',
+      "https://static.coindesk.com/wp-content/uploads/2019/01/shutterstock_1012724596-860x430.jpg",
+    text: "Awesome text for post 1",
     date: new Date().toJSON(),
-    booked: true
+    booked: true,
   },
   {
-    id: '2',
+    id: "2",
     img:
-      'https://cdn.londonandpartners.com/visit/general-london/areas/river/76709-640x360-houses-of-parliament-and-london-eye-on-thames-from-above-640.jpg',
-    text: 'Awesome text for post 2',
+      "https://cdn.londonandpartners.com/visit/general-london/areas/river/76709-640x360-houses-of-parliament-and-london-eye-on-thames-from-above-640.jpg",
+    text: "Awesome text for post 2",
     date: new Date().toJSON(),
-    booked: true
+    booked: true,
   },
   {
-    id: '3',
+    id: "3",
     img:
-      'https://static.coindesk.com/wp-content/uploads/2019/01/shutterstock_1012724596-860x430.jpg',
-    text: 'Awesome text for post 3',
+      "https://static.coindesk.com/wp-content/uploads/2019/01/shutterstock_1012724596-860x430.jpg",
+    text: "Awesome text for post 3",
     date: new Date().toJSON(),
-    booked: false
+    booked: false,
   },
   {
-    id: '4',
+    id: "4",
     img:
-      'https://cdn.londonandpartners.com/visit/general-london/areas/river/76709-640x360-houses-of-parliament-and-london-eye-on-thames-from-above-640.jpg',
-    text: 'Awesome text for post 4',
+      "https://cdn.londonandpartners.com/visit/general-london/areas/river/76709-640x360-houses-of-parliament-and-london-eye-on-thames-from-above-640.jpg",
+    text: "Awesome text for post 4",
     date: new Date().toJSON(),
-    booked: false
+    booked: false,
   },
   {
-    id: '5',
+    id: "5",
     img:
-      'https://static.coindesk.com/wp-content/uploads/2019/01/shutterstock_1012724596-860x430.jpg',
-    text: 'Awesome text for post 5',
+      "https://static.coindesk.com/wp-content/uploads/2019/01/shutterstock_1012724596-860x430.jpg",
+    text: "Awesome text for post 5",
     date: new Date().toJSON(),
-    booked: false
-  }
-]
+    booked: false,
+  },
+];
